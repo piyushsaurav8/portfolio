4cb9fb2325ac2842c725845aa85314b38814996d
RahulAnand-GitHub <rahul.anand88096@gmail.com>
2025-02-25 01:23:02 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#element', {
+  strings: ['Thumbnail Designer(adobe photoshop)'],
+  typeSpeed: 50,
+})
+
+var typed = new Typed('#molecule', {
+  strings: ['Video Editor(DaVinci Resolve)'],
+  typeSpeed: 50,
+  startDelay: 500,
+})
